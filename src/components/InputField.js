ff89dff958d9a4ca59e440ddd1768b96d16d6d05
Catia Costa <NORBERTO.CATIA@MacBook-Pro-de-Catia.local>
2020-06-26 16:27:40 +0100

    [PersonsFinder] - Part 2 : Filters styling and give them functionality.

---
+++ src/components/InputField.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Field= styled.div`
+    display: flex;
+    flex-direction: column;
+`
+const Label= styled.label`
+    font-family: var(--font-secondary);
+    font-weight: 700;
+    font-size: 18px;
+    padding-bottom: 5px;
+    padding-top: 20px;
+`
+const Input = styled.input`
+    border: 1px solid var(--colour-quaternary);
+    height: 40px;
+    padding: 1px 10px;
+    font-family: var(--font-secondary);
+    font-size: 16px;
+    font-weight: 400;
+`
+const Select = styled.select`
+    border: 1px solid var(--colour-quaternary);
+    height: 40px;
+    padding: 1px 10px;
+    font-family: var(--font-secondary);
+    font-size: 16px;
+    font-weight: 400;
+`
+const RadioItem = styled.div`
+    display: inline-block;
+    position: relative;
+    padding: 0 6px;
+    margin: 5px 0 0;
+  
+    input[type='radio'] {
+        display: none;
+    }
+
+    label {
+        padding-bottom: 10px;
+        font-family: var(--font-secondary);
+        font-size: 16px;
+        font-weight: 400;
+        text-transform: capitalize;
+    }
+
+    label:before {
+        content: " ";
+        display: inline-block;
+        position: relative;
+        top: 12px;
+        margin: 0 20px 0 0;
+        width: 33px;
+        height: 33px;
+        border-radius: 50%;
+        border: 2px solid var(--colour-quaternary);
+        background-color: transparent;
+    }
+
+    input[type=radio]:checked + label:after {
+        border-radius: 50%;
+        width: 25px;
+        height: 25px;
+        position: absolute;
+        top: 18px;
+        left: 12px;
+        content: " ";
+        display: block;
+        background: var(--colour-quaternary);
+    }
+`
+
+function InputField(props) {
+  return (
+    <Field>
+        <Label for={props.name}>{props.label}</Label>
+        {props.type==="text" &&
+            <Input type="text" id={props.name} name={props.name} onChange={props.onChange}/>
+        }
+        {props.type==="select" &&
+            <Select id={props.name} name={props.name} onChange={props.onChange}>
+                {props.options && props.options.map(option =>{
+                    return <option value={option}>{option}</option>    
+                })}
+            </Select>
+        }
+        {props.type==="radio" &&
+            props.options && props.options.map( (option, index) =>{
+                return (
+                    <RadioItem key={props.name + index} class="radio-item" onChange={props.onChange}>
+                        <input type="radio" id={props.name + index} name={props.name} value={option} />
+                        <label for={props.name + index}>{option}</label>
+                    </RadioItem>   
+                )
+            })
+        }
+    </Field>
+  );
+}
+
+export default InputField;
--- src/components/PersonsFinder.js
+++ src/components/PersonsFinder.js
@@ -2,15 +2,22 @@ import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import Person from './Person';
+import InputField from './InputField';
 
 const PersonsFinderSection = styled.section`
     display: flex;
     justify-content: flex-start;
     flex-wrap: wrap;
+    padding-top: 70px;
 `
 const Sidebar = styled.div`
     width: 100%;
+    margin-bottom: 50px;
+    
     @media (min-width: 768px){
+        max-width: 400px;
+    }
+    @media (min-width: 992px){
         width: 345px;
         margin-right: 55px;
     }
@@ -21,7 +28,7 @@ const Grid = styled.div`
     justify-content: space-between;
     flex-wrap: wrap;
     width: 100%;
-    @media (min-width: 768px){
+    @media (min-width: 992px){
         width: calc(100% - 400px);
     }
 `
@@ -36,28 +43,10 @@ function PersonsFinder() {
 
     const [posts, updatePosts] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [filterByName, setFilterByName] = useState("");
+    const [filterByAge, setFilterByAge] = useState("");
+    const [filterByGender, setFilterByGender] = useState("");
     
-
-    /*useEffect(() => {
-        axios.get('https://randomuser.me/api/?results=200')
-        .then(response =>{
-            const dataPosts = [...response.data.results];
-            console.log(dataPosts);
-            const updatedPosts = dataPosts.map( post =>{
-                return {
-                    name: post.name.first + " " + post.name.last, 
-                    gender: post.gender,
-                    email: post.email,
-                    picture: post.picture.medium,
-                    age: post.dob.age
-                }
-            });
-            updatePosts(updatedPosts);
-        });
-
-        
-    });*/
-
     useEffect( () => {
 
         axios({
@@ -74,28 +63,68 @@ function PersonsFinder() {
                     age: post.dob.age
                 }
             });
+
             updatePosts(updatedPosts);
             setLoading(false);
         });
     }, []);
+
+
+    let filteredPosts = [...posts];
+    if (filterByName !== ""){
+        filteredPosts = filteredPosts.filter(person =>
+            person.name.toLowerCase().includes(filterByName)
+        );
+    }
+    if (filterByGender !== ""){
+        filteredPosts = filteredPosts.filter(person =>
+            person.gender === filterByGender
+        );
+    }
+    if (filterByAge !== ""){
+        const AgeRange= filterByAge.split("-");
+        filteredPosts = filteredPosts.filter(person =>
+            person.age >= AgeRange[0] && person.age <= AgeRange[1]
+        );
+    }
                 
 
   return (
     <PersonsFinderSection>
         <Sidebar>
-
+            <h2>Refine</h2>
+            <InputField
+                label= "Name"
+                name="name"
+                type="text"
+                onChange={ event => setFilterByName(event.target.value)}
+            />
+            <InputField
+                label= "Age"
+                name= "age"
+                type="select"
+                onChange={ event => setFilterByAge(event.target.value)}
+                options= { ["","0-10", "10-20", "20-30", "30-40", "40-50"] }            
+            />
+            <InputField
+                label= "Gender"
+                name="gender"
+                type="radio"
+                onChange={ event => setFilterByGender(event.target.value)}
+                options = { ["male", "female"] }
+            />
         </Sidebar> 
     
         <Grid>
             { loading &&
-                <p>Loading...</p>
+                <h2>Loading...</h2>
             }
 
             { !loading &&
                 <div>
-                    <h2>{posts.length} results</h2>
+                    <h2>{filteredPosts.length} results</h2>
                     <GridWrapper>
-                    { posts.length >0  && posts.map( (post, index) => {
+                    { filteredPosts.length >0  && filteredPosts.map( (post, index) => {
                         return <Person 
                             key={index} 
                             name={post.name} 
@@ -103,7 +132,6 @@ function PersonsFinder() {
                             age={post.age}
                             email={post.email}
                             picture= {post.picture}
-                            //clicked={()=> this.postSelectHandler(post.id)} 
                             />
                         })
                     }
